b7149c8c0faaffbcb66f73301e58bcafd7a89f96
shakami <sepehr@mighty-macbook.home>
2020-02-23 23:13:25 -0800

    working state

--- src/app.js
+++ src/app.js
@@ -10,11 +10,27 @@
         $scope.myCheck = false;
         $scope.data = [];
         $scope.updateData = updateData;
+        $scope.editing = false;
+        $scope.toggleEdit = toggleEdit;
+        $scope.save = save;
+        $scope.newItem = { "value": false };
+        $scope.adding = false;
+        $scope.toggleAdd = toggleAdd;
+        $scope.add = add;
+        $scope.deleteItem = deleteItem;
+        
         
         getData();
         
         function getData() {
-            $http.get("https://api.jsonbin.io/b/5e536bcbd3c2f35597f6cb59/latest")
+           var req =
+            {
+                method: 'GET',
+                url: 'https://api.jsonbin.io/b/5e536bcbd3c2f35597f6cb59/latest',
+                headers: { 'Accept': 'application/json', 'Cache-Control': 'no-cache' }
+            };
+            
+            $http(req)
             .then(function(response) {
                 $scope.data = response.data;
             });
@@ -33,7 +49,38 @@
                 .then(function() {
                 getData();
             });
-            
+        }
+        
+        function toggleEdit() {
+            $scope.editing = !$scope.editing;
+        }
+        
+        function save() {
+            updateData();
+            toggleEdit();
+        }
+        
+        function toggleAdd() {
+            $scope.adding = !$scope.adding;
+        }
+        
+        function add() {
+            if($scope.newItem.name) {
+                $scope.data.push($scope.newItem);
+                updateData();
+                toggleAdd();
+            } else {
+                toggleAdd();
+            }
+        }
+        
+        function deleteItem(item) {
+            var i = $scope.data.indexOf(item);
+            if (i > -1) {
+                $scope.data.splice(i, 1);
+                updateData();
+            }
+            toggleEdit();    
         }
     }
     
